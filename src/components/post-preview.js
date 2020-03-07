0c015ee68d8877485c52b32d4b46c30600510f5d
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-03-06 17:03:45 -0700

    good state, but need fade when screen is smaller

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -3,11 +3,12 @@ import { css } from "@emotion/core"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
 import Img from "gatsby-image"
-import ReadLink from "./read-link"
+// import ReadLink from "./read-link"
 
 const PostHeadingLink = styled(Link)`
   text-decoration: none;
   color: #212225;
+  font-size: 1.125rem;
 `
 
 const PostPreview = ({ post }) => {
@@ -23,65 +24,56 @@ const PostPreview = ({ post }) => {
         }
       `}
     >
-      <h3>
-        <PostHeadingLink to={post.slug}>{post.title}</PostHeadingLink>
-      </h3>
-      <div
-        css={css`
-          display: flex;
-          align-items: flex-start;
-          margin-top: 1rem;
-        `}
-      >
-        <Link
-          to={post.slug}
+      <PostHeadingLink to={post.slug}>
+        <h3>{post.title}</h3>
+        <h5>Saia Fonua | March 06, 2020</h5>
+        <div
           css={css`
-            position: relative;
-            flex: 0 0 6.25rem;
-            align-self: flex-start;
-            width: 100px;
+            display: flex;
+            align-items: flex-start;
+            margin-top: 1rem;
           `}
         >
           <Img
             fluid={post.image.asset.fluid}
             css={css`
-              * {
-                margin-top: 0;
-              }
+              position: relative;
+              flex: 0 0 6.25rem;
+              align-self: flex-start;
+              margin-inline-end: 1rem;
             `}
-            style={{ width: "100px" }}
             alt={post.title}
           />
-        </Link>
-        <p
-          css={css`
-            position: relative;
-            overflow: hidden;
-            min-height: 2.5rem;
-            max-height: 6.375rem;
-            align-items: flex-start;
-          `}
-        >
-          {post.excerpt}
-        </p>
-        <span
-          css={css`
-            background: linear-gradient(
-              to bottom,
-              rgba(255, 255, 255, 0),
-              #fff
-            );
-            pointer-events: none;
-            display: block;
-            height: 2.5rem;
-            position: absolute;
-            right: 0;
-            left: 0;
-            bottom: 0;
-          `}
-        ></span>
-        {/* <ReadLink to={post.slug}>read this post &rarr;</ReadLink> */}
-      </div>
+          <p
+            css={css`
+              position: relative;
+              overflow: hidden;
+              min-height: 2.5rem;
+              max-height: 6.375rem;
+              align-items: flex-start;
+            `}
+          >
+            {post.excerpt}
+          </p>
+          {/* <span
+            css={css`
+              background: linear-gradient(
+                to bottom,
+                rgba(255, 255, 255, 0),
+                #fff
+              );
+              pointer-events: none;
+              display: block;
+              height: 2.5rem;
+              position: absolute;
+              right: 0;
+              left: 0;
+              bottom: 0;
+            `}
+          ></span> */}
+          {/* <ReadLink to={post.slug}>read this post &rarr;</ReadLink> */}
+        </div>
+      </PostHeadingLink>
     </article>
   )
 }
